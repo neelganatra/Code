65ae9cead60aa31c316b2e193bb97738963ccc79
neelganatra <45254603+neelganatra@users.noreply.github.com>
2018-11-22 13:13:03 +0530

    Add files via upload
    Printing Hello using multiple Main

---
+++ PRog1b.java
@@ -0,0 +1,14 @@
+class PRog1b{
+         public static void main(string [] args){
+                main(1);
+                main("java");
+       }
+      //method overridding
+      public static void main(int i){
+         system.out.println("Hello Tc3..");
+    }
+   //method overridding
+   public static void main(string str){
+     system.out.println("we are learning"+str);
+}
+}
