1af10a07559cc2427ae4f4588978dba3e1410ada
neelganatra <45254603+neelganatra@users.noreply.github.com>
2018-11-23 10:45:56 +0530

    Prog 4

---
+++ Marks.java
@@ -0,0 +1,18 @@
+public class Marks {
+	public static void main(String args[])
+{
+	int A=72,B=70,C=76,D=77,E=68,F=69;
+	float pr;
+
+		System.out.println("Marks of Subject 1: "+A);
+		System.out.println("Marks of Subject 2: "+B);
+		System.out.println("Marks of Subject 3: "+C);
+		System.out.println("Marks of Subject 4: "+D);
+		System.out.println("Marks of Subject 5: "+E);
+		System.out.println("Marks of Subject 6: "+F);
+
+		pr=(float)((A+B+C+D+E+F)/6.0);
+		System.out.println("Student Percentage: "+pr);
+	}
+}
+	
