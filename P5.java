ac1a09d17cec5f2842d28ce100a4ec84172aed17
neelganatra <45254603+neelganatra@users.noreply.github.com>
2018-11-28 10:01:46 +0530

    Program 5

---
+++ P5.java
@@ -0,0 +1,31 @@
+public class P5
+{
+	public static void main(String ard[])
+{
+	int choice=0;
+	choice = (int) (6*Math.random());
+	switch(choice)
+{
+	case 0:
+	System.out.println("Breakfast choice is dosa");
+	break;
+	case 1:
+	System.out.println("Breakfast choice is Maggi");
+	break;
+	case 2:
+	System.out.println("Brekfast choice is Milk");
+	break;
+	case 3:
+	System.out.println("Brekfast choice is sandwich");
+	break;
+	case 4:
+	System.out.println("Brekfast choice is Fafda Jalebi");
+	break;
+	case 5:
+	System.out.println("Brekfast choice is fruit");
+	break;
+	}	
+     }
+}
+
+	
