c086b5757e804c2d6ed83fc17a75964fab815f3c
neelganatra <45254603+neelganatra@users.noreply.github.com>
2018-11-30 09:48:45 +0530

    sorting extra program

---
+++ sort.java
@@ -0,0 +1,23 @@
+public class sort
+{
+	public static void main(String[] args)
+	{
+		int a[]={12,18,36,89,67,78}
+		int i,j,temp;
+		for(i=0;i<6;i++)
+		{
+			for(j=1+i;j<6;j++)
+			{
+				if(a[i]>a[j])
+				{
+					a=temp;
+					a=b;
+					b=temp;
+				}
+			}
+		System.out.println("Sorted Array is:" +a[i]);			
+
+
+		}
+	}
+}
