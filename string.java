f23c125225d233f808d922485560bfe64b8cf7ad
neelganatra <45254603+neelganatra@users.noreply.github.com>
2018-12-05 11:00:15 +0530

    Program 8

---
+++ string.java
@@ -0,0 +1,42 @@
+import java.util.Random;
+
+public class string
+{
+	public static void main(String args[])
+	{
+		int i;
+
+		Random r = new Random();
+		char c[] = new char[5];
+
+  		for(i=0; i<5; i++)
+		{
+			c[i]=(char)(r.nextInt(26)+ 'A');
+		}
+
+		for(i=0; i<5; i++)
+		{
+			switch(c[i])
+			{
+				case 'A' : System.out.println("A Is Vowel....!!!");
+				break;
+				
+				case 'E' : System.out.println("E Is Vowel....!!!");
+				break;
+
+				case 'I' : System.out.println("I Is Vowel....!!!");
+				break;
+
+				case 'O' : System.out.println("O Is Vowel....!!!");
+				break;
+
+				case 'U' : System.out.println("U Is Vowel....!!!");
+				break;
+
+				default:
+				System.out.println(c[i]);
+				break;
+			}
+		}
+	}
+}
