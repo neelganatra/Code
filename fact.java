2a3741813e63ab3fc2bb1ab53df0a696a161708e
neelganatra <45254603+neelganatra@users.noreply.github.com>
2018-12-05 10:31:27 +0530

    Extra(Convert string to int)

---
+++ fact.java
@@ -0,0 +1,18 @@
+public class  fact 
+{
+	public static void main(String args[])
+	{
+		int f=1;
+
+		String num;
+		num=args[0];
+
+		int no=Integer.parseInt(num);
+
+		for(int i=1; i<=no; i++)
+		{
+			f=f*i;
+		}
+		System.out.println("Fact="+f);
+	}
+}
